f37fcd91860b000300df046258fb1eec1bcda304
Solomon Yitayew <solyitayew7@gmail.com>
2026-04-01 14:53:55 +0300

    coress

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,24 @@ import gameRoutes from './routes/gameRoutes';
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: [
+    'https://bingo-game-front-end.vercel.app',
+    'http://localhost:3000',
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+// Handle preflight for all routes
+app.options('*', cors({
+  origin: [
+    'https://bingo-game-front-end.vercel.app',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 app.use(express.json());
 
